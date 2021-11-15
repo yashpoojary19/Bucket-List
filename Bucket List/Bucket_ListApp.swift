0fbeb71f99069f03b44ac64cf71dccd507551491
Yash Poojary <yashspoojary245@gmail.com>
2021-11-15 16:25:05 +0530

    Initial Commit

---
+++ Bucket List/Bucket_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bucket_ListApp.swift
+//  Bucket List
+//
+//  Created by Yash Poojary on 15/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Bucket_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
